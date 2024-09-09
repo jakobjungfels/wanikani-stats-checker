1b185d7b1a5093b451a3a8af208f57db669038e7
Jakob Jungfels <jakob.jungfels@lgln.niedersachsen.de>
2024-09-09 13:49:47 +0200

    added more info about the subjects of the highest reading and meaning streak
    Signed-off-by: Jakob Jungfels <jakob.jungfels@lgln.niedersachsen.de>

--- main.go
+++ main.go
@@ -19,22 +19,20 @@ func main() {
 	})
 	router.GET("/subjects/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		body := wanikani.WaniKaniGet(c, "https://api.wanikani.com/v2/subjects/"+id)
-		body_string := string(body)
+		body_string := wanikani.GetSubjects(c, id)
 		c.JSON(http.StatusOK, gin.H{
 			"message": string(body_string),
 		})
 	})
 	router.GET("/review_statistics/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		body := wanikani.WaniKaniGet(c, "https://api.wanikani.com/v2/review_statistics/"+id)
-		body_string := string(body)
+		body_string := wanikani.GetReviewStatistics(c, id)
 		c.JSON(http.StatusOK, gin.H{
 			"message": string(body_string),
 		})
 	})
 	router.GET("/review_statistics", func(c *gin.Context) {
-		body_string := wanikani.GetReviewStatistics(c)
+		body_string := wanikani.GetReviewStatistics(c, "")
 		database.AddReviewStatistics(body_string)
 		c.JSON(http.StatusOK, gin.H{
 			"message": string(body_string),
--- wanikani/wanikani.go
+++ wanikani/wanikani.go
@@ -11,12 +11,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func LongestStreak(c *gin.Context) (body_string string) {
-	body_string = GetReviewStatistics(c)
+func LongestStreak(c *gin.Context) (body_string_return string) {
+	body_string_return = GetReviewStatistics(c, "")
 
-	var highest_streak float64 = 0
+	var highest_streak_meaning float64 = 0
+	var highest_streak_meaning_id float64 = 0
+	var highest_streak_reading float64 = 0
+	var highest_streak_reading_id float64 = 0
 	var result map[string]interface{}
-	json.Unmarshal([]byte(body_string), &result)
+	json.Unmarshal([]byte(body_string_return), &result)
 
 	for key, value := range result {
 		switch value.(type) {
@@ -27,9 +30,16 @@ func LongestStreak(c *gin.Context) (body_string string) {
 						if key3 == "data" {
 							for key4, value4 := range value3.(map[string]interface{}) {
 								if key4 == "meaning_max_streak" {
-									if highest_streak < value4.(float64) {
-										highest_streak = value4.(float64)
-										fmt.Println("New highest streak", highest_streak)
+									if highest_streak_meaning < value4.(float64) {
+										highest_streak_meaning = value4.(float64)
+										highest_streak_meaning_id = value3.(map[string]interface{})["subject_id"].(float64)
+										fmt.Println("New highest streak meaning", highest_streak_meaning, "for subject", highest_streak_meaning_id)
+									}
+								} else if key4 == "reading_max_streak" {
+									if highest_streak_reading < value4.(float64) {
+										highest_streak_reading = value4.(float64)
+										highest_streak_reading_id = value3.(map[string]interface{})["subject_id"].(float64)
+										fmt.Println("New highest streak reading", highest_streak_reading, "for subject", highest_streak_reading_id)
 									}
 								}
 							}
@@ -39,18 +49,57 @@ func LongestStreak(c *gin.Context) (body_string string) {
 			}
 		}
 	}
-	body_string = strconv.FormatFloat(highest_streak, 'f', -1, 64)
+	body_string_return = strconv.FormatFloat(highest_streak_meaning, 'f', -1, 64)
+	body_string_return += "," + strconv.FormatFloat(highest_streak_meaning_id, 'f', -1, 64)
+	body_string_return += GetSubjectFields(c, highest_streak_meaning_id)
+
+	body_string_return += ";" + strconv.FormatFloat(highest_streak_reading, 'f', -1, 64)
+	body_string_return += "," + strconv.FormatFloat(highest_streak_reading_id, 'f', -1, 64)
+	body_string_return += GetSubjectFields(c, highest_streak_reading_id)
 
 	return
 }
 
-func GetReviewStatistics(c *gin.Context) (body_string string) {
-	body := WaniKaniGet(c, "https://api.wanikani.com/v2/review_statistics/")
-	body_string = string(body)
+func GetSubjectFields(c *gin.Context, id float64) (body_string_return string) {
+	body_string_subject := GetSubjects(c, strconv.FormatFloat(id, 'f', -1, 64))
+	var result_subject map[string]interface{}
+	json.Unmarshal([]byte(body_string_subject), &result_subject)
+	for key, value := range result_subject {
+		switch value.(type) {
+		case interface{}:
+			if key == "data" {
+				for key2, value2 := range value.(map[string]interface{}) {
+					if key2 == "meanings" || key2 == "readings" {
+						for _, value3 := range value2.([]interface{}) {
+							for key4, value4 := range value3.(map[string]interface{}) {
+								if key4 == "meaning" || key4 == "reading" {
+									body_string_return += "," + value4.(string)
+								}
+							}
+						}
+					} else if key2 == "characters" {
+						body_string_return += "," + value2.(string)
+					}
+				}
+			}
+		}
+	}
 	return
 }
 
-func WaniKaniGet(c *gin.Context, url string) (body []byte) {
+func GetSubjects(c *gin.Context, id string) (body_string_return string) {
+	body := Get(c, "https://api.wanikani.com/v2/subjects/"+id)
+	body_string_return = string(body)
+	return
+}
+
+func GetReviewStatistics(c *gin.Context, id string) (body_string_return string) {
+	body := Get(c, "https://api.wanikani.com/v2/review_statistics/"+id)
+	body_string_return = string(body)
+	return
+}
+
+func Get(c *gin.Context, url string) (body []byte) {
 	var bearer = "Bearer " + os.Getenv("WANIKANI_TOKEN")
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
